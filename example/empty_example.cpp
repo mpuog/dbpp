2eb428986d60d9d7559326a7328a16cc99896f20
Alex Titov <alex.v.titov@gmail.com>
2023-07-07 18:05:32 +0300

    Tune build to VS 2019

--- example/empty_example.cpp
+++ example/empty_example.cpp
@@ -1,4 +1,38 @@
-auto main() -> int
+
+
+#ifdef _WIN32
+#  include <Windows.h>
+#endif  // WIN32
+
+#include <iostream>
+
+int main()
 {
+#ifdef _WIN32
+  SetConsoleOutputCP(65001);
+  SetConsoleCP(65001);
+#endif  // WIN32
+  printf(">>> ");
+  std::cout << __DATE__ " " __TIME__ "  "
+#ifdef _MSC_VER
+            << "MS:" << _MSC_VER
+#else
+            << "GCC:" << __GNUC__
+#endif
+
+#ifdef _WIN32
+            << " WINDOWS "
+#else
+            << " LINUX "
+#endif
+
+#ifdef _DEBUG
+            << " DEBUG "
+#else
+            << " RELEASE(MAY BE) "
+#endif
+
+            << "\n";
+
   return 0;
 }
